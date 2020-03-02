fb22d53547846d4d8dc2193c8c6f59d55129cff6
Santiago <santiagocantu98@gmail.com>
2020-03-01 19:46:30 -0600

    uploading work done in GraphQL course

---
+++ job-board/client/src/requests.js
@@ -0,0 +1,141 @@
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost';
+import gql from 'graphql-tag';
+import { isLoggedIn, getAccessToken } from "./auth";
+
+const endpointURL = 'http://localhost:9000/graphql';
+
+const authLink = new ApolloLink((operation, forward) => {
+    if (isLoggedIn()) {
+        //request.headers['authorization'] = 'Bearer ' + getAccessToken();
+        operation.setContext({
+            headers: {
+                'authorization': 'Bearer ' + getAccessToken()
+            }
+        })
+    }
+    return forward(operation);
+})
+
+const client = new ApolloClient({
+    link: ApolloLink.from([
+        authLink,
+        new HttpLink({uri: endpointURL})
+    ]),
+    cache: new InMemoryCache()
+});
+
+/*async function graphqlRequest(query, variables={}) {
+    const request = {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({query, variables})
+    }
+    if (isLoggedIn()) {
+        request.headers['authorization'] = 'Bearer ' + getAccessToken();
+    }
+
+    const response = await fetch(endpointURL, request);
+    const responseBody = await response.json();
+    if (responseBody.errors) {
+        const message = responseBody.errors.map((error) => error.message).join('\n');
+        throw new Error(message);
+    }
+    return responseBody.data;
+}
+*/
+
+const jobQuery = gql`{
+    jobs {
+        id
+        title
+        company {
+            id
+            name
+        }
+    }
+}`;
+
+export async function createJob(input) {
+    const mutation = gql`
+        mutation CreateJob($input: CreateJobInput){
+            job: createJob(input: $input) {
+                id
+                title
+                company {
+                    id
+                    name
+                }
+            }
+        }
+    `;
+    const {data: {job}} = await client.mutate({
+        mutation,
+        variables: {input}
+        /*update: (cache, {data}) => {
+            cache.writeQuery({
+                query: jobQuery,
+                variables: {id: data.job.id},
+                data
+            })
+        }
+        */
+    });
+    return job;
+}
+
+export async function loadCompany(id){
+    const query = gql`
+        query CompanyQuery($id: ID!){
+            company(id: $id) {
+            id
+            name
+            description
+            jobs {
+                id
+                title
+            }
+            }
+        }
+    `;
+      const {data: {company}} = await client.query({query, variables: {id}});
+      return company;
+}
+
+export async function loadJob(id) {
+    const query = gql`
+        query JobQuery($id: ID!) {
+            job(id: $id) {
+                id
+                title
+                company {
+                    id
+                    name
+                }
+                description
+            }
+        }
+    `;
+    const {data: {job}} = await client.query({query, variables: {id}});
+    /*const {data: {job}} = await client.query({
+        query: jobQuery,
+        variables: {id}
+    });
+    */
+    return job;
+}
+
+export async function loadJobs() {
+    const query = gql`{
+        jobs {
+            id
+            title
+            company {
+                id
+                name
+            }
+        }
+    }`;
+    const {data: {jobs}} = await client.query({query, fetchPolicy: 'no-cache'});
+    return jobs;
+}
+
